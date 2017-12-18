b28718d6369121121dfb2876c98300cd199ee394
Sean Braithwaite <brapse@gmail.com>
2017-12-18 19:32:47 +0100

    Simplify pipeline API
      + Switch to simple functions which process iterators

--- aleph_ner/pipeline.py
+++ aleph_ner/pipeline.py
@@ -16,62 +16,32 @@ from collections import defaultdict
      "entities": [["ORGANIZATION", "US. Department Of Immigration"]]
     }
 """
-class Pipe:
-    def __iter__(self):
-        return self.rawIter
 
-    def __next__(self):
-        self.rawIter.next()
+def AlephDumpReader(path):
+    with open(path, 'rU') as f:
+        for line in f:
+            doc = json.loads(line)
+            for unwanted_key in set(doc.keys()) - set(['text', 'document_id']):
+                del doc[unwanted_key]
+            yield(doc)
 
-    def __str__(self):
-        self.name
+def Tokenizer(reader):
+    for doc in reader:
+        doc['tokens'] = word_tokenize(doc['text'])
+        yield doc
 
-class AlephDumpReader(Pipe):
-    """
-    Takes in json documents, produces a dict with 'text' and 'document_id'
-    """
-    def __init__(self, filepath):
-        def readAlephDump(path):
-            with open(path, 'rU') as f:
-                for line in f:
-                    doc = json.loads(line)
-                    for unwanted_key in set(doc.keys()) - set(['text', 'document_id']):
-                        del doc[unwanted_key]
-                    yield(doc)
-        self.name = "AlephDump(%s)" % filepath
-        self.rawIter = readAlephDump(filepath)
+def Annotator(reader):
+    for doc in reader:
+        doc['entities'] = ne_chunk(pos_tag(doc['tokens']))
+        yield doc
 
-class Tokenizer(Pipe):
-    def __init__(self, reader):
-        def tokenizeDoc():
-            for doc in reader:
-                doc['tokens'] = word_tokenize(doc['text'])
-                yield doc
-        self.rawIter = tokenizeDoc()
-        self.name = "nltk_word_tokenize"
 
-class Annotator(Pipe):
-    def __init__(self, reader):
-        def annotateDoc(reader):
-            for doc in reader:
-                doc['entities'] = ne_chunk(pos_tag(doc['tokens']))
-                yield doc
-
-        self.rawIter = annotateDoc(reader)
-        self.name = "nltk_ne_chunk"
-
-class Reporter(Pipe):
-    def __init__(self, reader):
-        def genReporting(reader):
-            for doc in reader:
-                formatted = defaultdict(list)
-                for ent in doc['entities']:
-                    if isinstance(ent, Tree):
-                        entLabel = str.join(" ", [x for x,y in ent.flatten()])
-                        formatted[ent.label()].append(entLabel)
-                doc['cleaned_entities'] = dict(formatted)
-                yield doc
-
-        # organize entities
-        self.rawIter = genReporting(reader)
-        self.name="aggregate_reporter"
+def Reporter(reader):
+    for doc in reader:
+        formatted = defaultdict(list)
+        for ent in doc['entities']:
+            if isinstance(ent, Tree):
+                entLabel = str.join(" ", [x for x,y in ent.flatten()])
+                formatted[ent.label()].append(entLabel)
+        doc['cleaned_entities'] = dict(formatted)
+        yield doc
